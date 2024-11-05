0c17c1f4fce6d497137fe02cb85716d1bd6215c7
Antonio-A-N-J <antonionunes710@gmail.com>
2024-11-05 18:48:07 -0300

    Todos os ToDo foram resolvidos.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
+Console.WriteLine("Celular 1.");
+Iphone c1 = new Iphone(numero: "12345", modelo: "Iphone", imei: "abc@def", memoria: 34);
+c1.Ligar();
+c1.InstalarAplicativo("YouTube");
+
+Console.WriteLine("Celular 2.");
+Nokia c2 = new Nokia(numero: "6789", modelo: "Nokia", imei: "ghi@jklm", memoria: 34);
+c2.Ligar();
+c2.InstalarAplicativo("YouTube");
+
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
